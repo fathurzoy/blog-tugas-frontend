cef296ab933af214b83ac85205967e251279366d
fathurzoy <fathurrahmanf123@gmail.com>
2021-09-26 12:04:25 +0700

    fix

--- src/pages/Admin/Login/index.js
+++ src/pages/Admin/Login/index.js
@@ -49,7 +49,7 @@ class Login extends React.Component {
 
           // If user data is stored in the session Storage, then reload page to update Navbar component appropriately
           if (count === 1) {
-            window.location.reload();
+            // window.location.reload();
           }
         }
         // If user data returned is invalid, then redirect to the login page once again
@@ -60,47 +60,10 @@ class Login extends React.Component {
       .catch((err) => console.error(err));
   }
 
-  // Twitter login success callback
-  // Very similar to the previous google login callback
-  twitterAuthHandler = (err, data) => {
-    if (err) {
-      window.location = "/fathurzoy/login";
-    } else {
-      const user = {
-        username: data.screen_name,
-        socialId: data.user_id,
-      };
-
-      axios
-        .post(`${baseURL}/auth/login`, user)
-        .then((res) => {
-          let count = 0;
-
-          if (res.data.socialId) {
-            sessionStorage.setItem("isLoggedIn", "true");
-            sessionStorage.setItem("username", res.data.username);
-            count++;
-
-            window.setTimeout(() => {
-              sessionStorage.removeItem("isLoggedIn");
-              sessionStorage.removeItem("username");
-            }, 24 * 60 * 60 * 60);
-
-            if (count === 1) {
-              window.location.reload();
-            }
-          } else {
-            window.location = "/fathurzoy/login";
-          }
-        })
-        .catch((err) => console.error(err));
-    }
-  };
-
   // Google login failure callback
   failureGoogleLogin(response) {
     console.error(response);
-    window.location = "/fathurzoy/login";
+    // window.location = "/fathurzoy/login";
   }
 
   render() {
